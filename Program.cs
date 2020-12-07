0eaa27f8ebf0f0cd0c06df229d9c3a4e412748b9
Jonas Granquist <jonas.granqvist@gmail.com>
2020-12-07 19:29:55 +0100

    Up to day 3 part 1 answers

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace advent_of_code_2020_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day1.Run();
+            Day2.Run();
+            Day3.Run();
+        }
+    }
+}
